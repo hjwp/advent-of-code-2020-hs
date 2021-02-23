9dabe8a1c748ec6788502810e40eac3f4333fcfd
Harry <hjwp2@cantab.net>
2021-02-23 23:09:52 +0000

    day 1 done

--- Advent.hs
+++ Advent.hs
@@ -1,4 +1,10 @@
 module Advent where
 
 fixExpenses :: [Int] -> Int
-fixExpenses x = head x
+fixExpenses xs = head [ x * y | x <- xs, y <- xs, x /= y, x + y == 2020]
+
+main :: IO ()
+main = do
+    someExpenses <- getContents
+    let result = fixExpenses $ map read $ [ x | x <- lines someExpenses, x /= ""]
+    print result
